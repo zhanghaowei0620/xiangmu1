e5e17474c5f5c6d92b85d342133631640c5f916b
hahaha <you@example.com>
2020-10-16 15:48:33 +0800

    哈哈哈哈

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -2,7 +2,11 @@
 
 return [
     'autoload' => false,
-    'hooks' => [],
+    'hooks' => [
+        'config_init' => [
+            'nkeditor',
+        ],
+    ],
     'route' => [],
     'priority' => [],
 ];
